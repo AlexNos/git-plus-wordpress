e2f1934b74ea13ea746c6564a8fd6344e2dbc89d
Jeff Chown <jchown@successtech.com>
2013-05-26 10:46:06 -0400

    added indicator in footer

--- wp-content/themes/wpbootstrap-child/footer.php
+++ wp-content/themes/wpbootstrap-child/footer.php
@@ -4,7 +4,7 @@
 		  <div class="container-fluid">
 		    <div class="row-fluid">
 		      <div class="span12">
-		      	<h4 class="pull-right">Copyright (c) 2013 HorseFly International</h4>
+		      	<h4 class="pull-right">MASTER BRANCH - Copyright (c) 2013 HorseFly International</h4>
 		      </div>
 		    </div>
 		  </div>
